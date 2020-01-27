1913f1d4cf762bd0169e51b6dda1ddcebed2f1d0
J21ongcol <59629515+J21ongcol@users.noreply.github.com>
2020-01-26 22:18:59 -0800

    Create test-sketch.js

---
+++ test-sketch.js
@@ -0,0 +1,64 @@
+
+function setup() {
+
+        createCanvas(600, 300);
+
+        rectMode(CENTER);
+
+}
+
+
+function draw() {
+
+        background(1, 111, 98);
+
+
+        // declaration of variables
+
+        var x = 200;
+
+        var y = 150;
+
+       // rectangle
+  
+        fill (140);
+  
+        rect (x, 200, 30, 200);
+
+
+        // circle
+
+        fill(250, 34, 93)
+
+        ellipse(x, y, 170, 170);
+
+
+        // rectangle
+
+        fill(255);
+
+        rect(x, y, 120, 25);
+  
+        // rectangle
+fill (15, 100, 240);
+  rect (450, 166, 90, 38);
+        // rectangle
+fill (255)
+  rect (450, 166, 76, 28);
+        // rectangle
+fill (15, 100, 240);
+  rect (450, 200, 100, 43);
+        // rectangle
+fill (0);
+  rect (420, 226.5, 13, 10);
+        // rectangle
+fill (0);
+  rect (480, 226.5, 13, 10);
+        // cicle
+fill (255, 220, 0);
+  ellipse (477, 200, 15, 15);
+        // cicle
+fill (255, 220, 0);
+  ellipse (425, 200, 15, 15)
+
+}
